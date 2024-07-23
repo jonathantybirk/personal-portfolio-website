068a426e6cf8e852abf63c3a8d9487d0a281098f
jonathantybirk <jonathantybirk@gmail.com>
2024-07-24 00:47:09 +0200

    update

---
+++ frontend/src/components/projects/orbiter/Orbiter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Orbiter.css';
+
+const Orbiter: React.FC = () => {
+    return (
+        <div className="orbiter">
+            <h1>Orbiter Project</h1>
+            <p>Description of the Orbiter project...</p>
+        </div>
+    );
+};
+
+export default Orbiter;
