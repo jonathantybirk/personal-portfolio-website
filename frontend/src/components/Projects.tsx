57be447681d7805052d045789ce77b9ce9fa4625
jonathantybirk <jonathantybirk@gmail.com>
2024-07-30 03:37:05 +0200

    update

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Route, Routes, Link, Navigate } from 'react-router-dom';
 import "./Projects.css";
 import { projects } from '../data/projectsData';
+import Project from './ProjectPage';
+
 
 const Projects = () => {
     return (
         <div className="projects">
-            {projects.map(project => (
+            {projects.map((project, index) => (
                 <div key={project.id}>
                     <br />
                     <img src={project.image} alt={project.title} />
@@ -15,11 +17,19 @@ const Projects = () => {
                     <p>{project.shortDescription}</p>
                     <br />
                     <p><i>{project.dates}</i></p>
-                    <hr />
+                    {index !== projects.length - 1 ? <hr /> : <br />}
                 </div>
             ))}
         </div>
     );
 };
+
+export const ProjectRoutes = () => (
+    <Routes>
+        <Route path=":projectId" element={<Project />} />
+        <Route path=":projectId/*" element={<Navigate to="" replace />} />
+        <Route path="*" element={<Projects />} />
+    </Routes>
+);
 
 export default Projects;
