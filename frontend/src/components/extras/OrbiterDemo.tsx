513b282f59c8597d49c6d48599993851da7a488b
jonathantybirk <jonathantybirk@gmail.com>
2024-09-04 23:35:31 +0200

    update

--- frontend/src/components/extras/OrbiterDemo.tsx
+++ frontend/src/components/extras/OrbiterDemo.tsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 const OrbiterDemo: React.FC = () => {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
     useEffect(() => {
         document.title = "Orbiter";
         document.body.style.overflowY = "hidden";
+        if (iframeRef.current) {
+            iframeRef.current.focus();
+        }
+        
         return () => {
             document.body.style.overflowY = "scroll";
         };
@@ -12,8 +17,10 @@ const OrbiterDemo: React.FC = () => {
     
     return (
         <iframe
+        ref={iframeRef}
         src={"/website-components/orbiter-demo/index.html"}
         style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100vh", border: "none", zIndex: 9999}}
+        tabIndex={-1}
         />
     );
 }
