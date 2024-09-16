cac2fc93eac3c01665609211d837298b1f7df9e2
jonathantybirk <jonathantybirk@gmail.com>
2024-09-17 00:31:58 +0200

    update

--- src/components/extras/Resume.tsx
+++ src/components/extras/Resume.tsx
@@ -10,10 +10,14 @@ const Resume: React.FC = () => {
 }, []);
     
     return (
-        <iframe
-          src={"/website-components/resume.pdf"}
-          style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100vh", border: "none", zIndex: 9999 }}
-        />
+        <div>
+            Resume currently unavailable. Check out my LinkedIn at <b><a href="https://www.linkedin.com/in/jonathantybirk/" target="_blank" rel="noopener noreferrer">linkedin.com/in/jonathantybirk</a></b>↗.
+        </div>
+
+        // <iframe
+        //   src={"/website-components/resume.pdf"}
+        //   style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100vh", border: "none", zIndex: 9999 }}
+        // />
     );
 }
 
