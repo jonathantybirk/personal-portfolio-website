138bca01596bb2d520aa961f1061b165fed7ea1e
Jonathan Tybirk <jonathantybirk@gmail.com>
2024-12-25 21:49:12 +0100

    Refactor project pages
    Added interfaces, split components into subcomponents, gave more appropriate component names
    
    lil edit on index.tsx for consistency

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,38 +1,52 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes, Link, Navigate } from 'react-router-dom';
 import "./Projects.css";
 import projects from './data/projectsData';
-import Project from './ProjectPage';
+import ProjectPage from './ProjectPage';
+
+interface ProjectSummary {
+  id: string;
+  image_path: string;
+  title: string;
+  developmentTools: string;
+  shortDescription: string;
+  dates: string;
+}
+
+const ProjectCard: React.FC<{ project: ProjectSummary; isLast: boolean }> = ({ project, isLast }) => (
+  <div key={project.id}>
+    <br />
+    <img src={project.image_path} alt={project.title} />
+    <h2><u><Link to={`/projects/${project.id}`}>{project.title}</Link></u></h2>
+    <p><i>{project.developmentTools}</i></p>
+    <p>{project.shortDescription}</p>
+    <br />
+    <p><i>{project.dates}</i></p>
+    {!isLast && <hr />}
+  </div>
+);
 
 const Projects: React.FC = () => {
-    useEffect(() => {
-        document.title = "Projects | Portfolio";
-      }, []);
+  useEffect(() => {
+    document.title = "Projects | Portfolio";
+  }, []);
 
-    return (
-        <div className="projects">
-            {projects.map((project, index) => (
-                <div key={project.id}>
-                    <br />
-                    <img src={project.image_path} alt={project.title} />
-                    <h2><u><Link to={`/projects/${project.id}`}>{project.title}</Link></u></h2>
-                    <p><i>{project.developmentTools}</i></p>
-                    <p>{project.shortDescription}</p>
-                    <br />
-                    <p><i>{project.dates}</i></p>
-                    {index !== projects.length - 1 ? <hr /> : <br />}
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div className="projects">
+      {projects.map((project, index) => (
+        <ProjectCard key={project.id} project={project} isLast={index === projects.length - 1} />
+      ))}
+    </div>
+  );
 };
 
-export const ProjectRoutes = () => (
-    <Routes>
-        <Route path="/:projectId" element={<Project />} />
-        <Route path="/:projectId/*" element={<Navigate to="" replace />} />
-        <Route path="/*" element={<Projects />} />
-    </Routes>
+const ProjectRoutes: React.FC = () => (
+  <Routes>
+    <Route path="/:projectId" element={<ProjectPage />} />
+    <Route path="/:projectId/*" element={<Navigate to="" replace />} />
+    <Route path="/*" element={<Projects />} />
+  </Routes>
 );
 
 export default Projects;
+export { ProjectRoutes };
