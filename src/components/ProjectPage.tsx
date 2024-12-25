fe084c42523524b5dd951ca1b1e638253eda24d7
Jonathan Tybirk <jonathantybirk@gmail.com>
2024-12-25 21:49:12 +0100

    Refactor project pages
    Added interfaces, split components into subcomponents, gave more appropriate component names
    
    lil edit on index.tsx for consistency

--- src/components/ProjectPage.tsx
+++ src/components/ProjectPage.tsx
@@ -1,36 +1,39 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useParams, Navigate } from 'react-router-dom';
 import "./ProjectPage.css";
 import projects from './data/projectsData';
 
-const Project = () => {
-    useEffect(() => {
-        document.title = "Projects | Portfolio";
-      }, []);
+interface ProjectDetail {
+  id: string;
+  image_path: string;
+  title: string;
+  projectDescription: string;
+}
 
-    const { projectId } = useParams();
-    const project = projects.find(p => p.id === projectId);
+const ProjectPage: React.FC = () => {
+  useEffect(() => {
+    document.title = "Projects | Portfolio";
+  }, []);
 
-    if (!project) {
-        return <Navigate to="/projects" replace />;
-    }
+  const { projectId } = useParams<{ projectId: string }>();
+  const project: ProjectDetail | undefined = projects.find((p) => p.id === projectId);
 
-    // useEffect(() => {
-    //     document.title = `${project.title} | Portfolio`;
-    //   }, []);
+  if (!project) {
+    return <Navigate to="/projects" replace />;
+  }
 
-    return (
-        <div className="project-page">
-            <div key={project.id}>
-                <br />
-                <img src={project.image_path} alt={project.title} />
-                <div className="content">
-                    <h1>{project.title}</h1>
-                    <div dangerouslySetInnerHTML={{ __html: project.projectDescription }}></div>
-                </div>
-            </div>
+  return (
+    <div className="project-page">
+      <div key={project.id}>
+        <br />
+        <img src={project.image_path} alt={project.title} />
+        <div className="content">
+          <h1>{project.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: project.projectDescription || '' }}></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Project;
+export default ProjectPage;
